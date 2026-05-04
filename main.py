02a3bd60c61aaabab474e52fe1b4aaf78c2fcbbc
sharvani <sharvani.komuravelly@zemosolabs.com>
2026-05-04 20:03:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(a: int, b: int) -> int:
+    """Return sum of two numbers."""
+    return a + b
+
+
+def main() -> None:
+    result = add(2, 3)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
